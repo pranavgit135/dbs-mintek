c2617f204090d902d822806894c376561530aac3
pranavgit135 <khandekarpranav52@gmail.com>
2025-07-13 19:17:15 +0530

    Update about-section.tsx

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -104,8 +104,8 @@ function AnimatedStat({ stat, isVisible }: { stat: (typeof stats)[0]; isVisible:
   )
 }
 
- const url = `${process.env.NEXT_PUBLIC_BASE_URL}/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries?access_token=${process.env.NEXT_PUBLIC_ACCESS_TOKEN}&content_type=aboutSection`;
-console.log(url)
+ const url = `https://cdn.contentful.com/spaces/3iacs7izb5ma/environments/master/entries?access_token=YHoprnmLTn1fYI1cCiwdQDb8QNCwP-pFxG923-a79vc&content_type=aboutSection`;
+
 
 export default  function AboutSection() {
   const [isVisible, setIsVisible] = useState(false)
