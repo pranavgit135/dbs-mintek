94652773a5f36b62f72a7a7c9c62e2459e713a6d
pranavgit135 <khandekarpranav52@gmail.com>
2025-07-17 14:05:06 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'mintek',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  description: '',
+  generator: 'GEOSoftech',
 }
 
 export default function RootLayout({
